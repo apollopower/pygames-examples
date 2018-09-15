5007a972e680c556279ef28ae319db9884c5b1d7
Jonas Erthal <erthaljonas@gmail.com>
2018-09-15 11:18:26 -0400

    Resource systems/ moving clouds

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from pygame.locals import *
 #init the pygame module:
 pygame.init()
 
+# clock to manage frames
+fpsClock = pygame.time.Clock()
+
+# __________________________________________________
 
 # PLAYER DATA
 PLAYER = pygame.image.load("resources/player.png")
 # The position of the player, X & Y
 playerPos = [0,0]
+
+# Constants representing colors:
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# __________________________________________________
+
+# RESOURCES
 
 
 # Constants representing different resources:
@@ -22,6 +34,21 @@ WATER = 2
 COAL = 3
 ROCK = 4
 LAVA = 5
+CLOUD = 6
+
+
+# Inventory:
+inventory = {
+    DIRT: 0,
+    GRASS: 0,
+    WATER: 0,
+    COAL: 0,
+    ROCK: 0,
+    LAVA: 0
+}
+
+# Font for inventory:
+INVFONT = pygame.font.SysFont('HelveticaBold.ttf', 24)
 
 # A dictionary linking resources to textures:
 textures = {
@@ -30,7 +57,8 @@ textures = {
     WATER: pygame.image.load("resources/water.png"),
     COAL: pygame.image.load("resources/coal.png"),
     ROCK: pygame.image.load("resources/rock.png"),
-    LAVA: pygame.image.load("resources/lava.png")
+    LAVA: pygame.image.load("resources/lava.png"),
+    CLOUD: pygame.image.load("resources/cloud.png")
 }
 
 # Game Dimensions:
@@ -46,10 +74,11 @@ resources = [DIRT, GRASS, WATER, COAL, ROCK, LAVA]
 tilemap = [[DIRT for w in range(MAPWIDTH)] for h in range(MAPHEIGHT)]
 
 # Create a new drawing surface
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT * TILESIZE))
+DISPLAYSURF = pygame.display.set_mode((MAPWIDTH * TILESIZE, MAPHEIGHT * TILESIZE + 80))
 
-# Give the window a caption:
-pygame.display.set_caption("Chinese Minecraft")
+# Give the window a caption, and icon:
+pygame.display.set_caption("SquareCraft")
+pygame.display.set_icon(textures[GRASS])
 
 
 # Generating Game World:
@@ -57,7 +86,7 @@ pygame.display.set_caption("Chinese Minecraft")
 for rw in range(MAPHEIGHT):
     # Loop through each column of row
     for cl in range(MAPWIDTH):
-        randomNumber = random.randint(0,50)
+        randomNumber = random.randint(0,60)
         # if 0, make it lava
         if randomNumber == 0:
             tile = LAVA
@@ -75,8 +104,26 @@ for rw in range(MAPHEIGHT):
         tilemap[rw][cl] = tile
 
 
+# _________________________________________________________
+
+# Cloud starting X Position on screen:
+cloud1X = -200
+cloud1Y = 0
+
+cloud2X = -250
+cloud2Y = random.randint(0, MAPHEIGHT*TILESIZE)
+
+cloud3X = -350
+cloud3Y = random.randint(0, MAPHEIGHT*TILESIZE)
+
+
+# __________________________________________________________
+# GAME
+
 # Loop for game:
 while True:
+    # Resetting screen to black to avoid visual issues
+    DISPLAYSURF.fill(BLACK)
     # Get all the user events;
     for event in pygame.event.get():
         # If user presses close window button:
@@ -86,14 +133,48 @@ while True:
             sys.exit()
         # If a key is pressed:
         elif event.type == KEYDOWN:
+            # ____________________________________________________________
+            # MOVEMENT
             if event.key == K_d:
-                playerPos[0] += 1
+                if playerPos[0] < (MAPWIDTH - 1): playerPos[0] += 1
             elif event.key == K_a:
-                playerPos[0] -= 1
+                playerPos[0] -= 1 if playerPos[0] > 0 else 0
             elif event.key == K_w:
-                playerPos[1] -= 1
+                playerPos[1] -= 1 if playerPos[1] > 0 else 0
             elif event.key == K_s:
-                playerPos[1] += 1
+                if playerPos[1] < (MAPHEIGHT - 1): playerPos[1] += 1
+            # _____________________________________________________________
+            # Mining/adding to inventory
+            if event.key == K_SPACE:
+                # What resource is the player standing on?
+                currentTile = tilemap[playerPos[1]][playerPos[0]]
+                # If for inventory, Player now has one more of the resource:
+                if currentTile in inventory:
+                    inventory[currentTile] += 1
+                    # After mining, the player is standing on dirt:
+                    tilemap[playerPos[1]][playerPos[0]] = DIRT
+                    print(inventory[currentTile])
+            # ______________________________________________________________
+            # Placing Down Blocks
+            if event.key == K_1 and inventory[DIRT] > 0:
+                inventory[DIRT] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = DIRT
+            elif event.key == K_2 and inventory[GRASS] > 0:
+                inventory[GRASS] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = GRASS
+            elif event.key == K_3 and inventory[WATER] > 0:
+                inventory[WATER] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = WATER
+            elif event.key == K_4 and inventory[COAL] > 0:
+                inventory[COAL] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = COAL
+            elif event.key == K_5 and inventory[ROCK] > 0:
+                inventory[ROCK] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = ROCK
+            elif event.key == K_6 and inventory[LAVA] > 0:
+                inventory[LAVA] -= 1
+                tilemap[playerPos[1]][playerPos[0]] = LAVA
+
     
     # End of event checker:
 
@@ -102,8 +183,41 @@ while True:
         for column in range(MAPWIDTH):
             # Draw the resource at that position in the tilemap
             DISPLAYSURF.blit(textures[tilemap[row][column]], (column * TILESIZE, row * TILESIZE))
+    
+    # Display Cloud, moving it slightly each frame:
+ 
+    DISPLAYSURF.blit(textures[CLOUD].convert_alpha(), (cloud1X, cloud1Y))
+    cloud1X += 1
+    if cloud1X > MAPWIDTH*TILESIZE:
+        cloud1X = -200
+        cloud1Y = random.randint(0,MAPHEIGHT*TILESIZE)
+    
+    DISPLAYSURF.blit(textures[CLOUD].convert_alpha(), (cloud2X, cloud2Y))
+    cloud2X += 1
+    if cloud2X > MAPWIDTH*TILESIZE:
+        cloud2X = -200
+        cloud2Y = random.randint(0,MAPHEIGHT*TILESIZE)
+
+    DISPLAYSURF.blit(textures[CLOUD].convert_alpha(), (cloud3X, cloud3Y))
+    cloud3X += 1
+    if cloud3X > MAPWIDTH*TILESIZE:
+        cloud3X = -200
+        cloud3Y = random.randint(0,MAPHEIGHT*TILESIZE)
+
+    
+    # Display Inventory (starting 10 pixels in):
+    placePosition = 10
+    for item in resources:
+        # Add the image
+        DISPLAYSURF.blit(textures[item],(placePosition, MAPHEIGHT*TILESIZE + 20))
+        placePosition += 50
+        # Add the text showing amount in inventory:
+        textObj = INVFONT.render(str(inventory[item]), True, WHITE, BLACK)
+        DISPLAYSURF.blit(textObj, (placePosition, MAPHEIGHT*TILESIZE + 20))
+        placePosition += 80
 
     # Display player in correct position:
     DISPLAYSURF.blit(PLAYER, (playerPos[0] * TILESIZE, playerPos[1] * TILESIZE))
     # update the display:
     pygame.display.update()
+    fpsClock.tick(24)
